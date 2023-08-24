a786522ad22451f3c51ece2bf69662f25831cfa1
kleos <36254493+amlitio@users.noreply.github.com>
2023-08-24 15:11:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,75 @@
-
 import streamlit as st
 import pandas as pd
 import openpyxl as px
 import openai
 
-# OpenAI API Key (Replace with your actual key)
+# OpenAI API Key
 openai_api_key = "YOUR_OPENAI_API_KEY"
 
-# Function to analyze data with OpenAI
-def analyze_data_with_openai(data):
-    data_text = data.to_string()
-    prompt = f"Analyze the following business data and provide insights, budget analysis, and forecasting:\n{data_text}"
-    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=150)
-    analysis_result = response.choices[0].text
-    return analysis_result
-
-# Function to review Excel file
 def review_excel(file):
-    workbook = px.load_workbook(file)
-    sheet = workbook.active
-    data = pd.DataFrame(sheet.values)
-    data.columns = data.iloc[0]
-    data = data[1:]
-    analysis_result = analyze_data_with_openai(data)
-    return analysis_result
+    # Read the Excel file and perform a basic review
+    df = pd.read_excel(file)
+    return df.describe()
 
-# Function to check formulas and provide feedback
 def check_formulas(file):
+    # Check for formula errors in the Excel file
     workbook = px.load_workbook(file)
     sheet = workbook.active
     feedback = []
+    # Example: Check for specific formulas and provide feedback
+    return feedback
+
+def suggest_python_integration(file):
+    workbook = px.load_workbook(file)
+    sheet = workbook.active
+    suggestions = []
+
+    # Analyzing existing formulas and data
     for row in sheet.iter_rows():
         for cell in row:
             if cell.value and isinstance(cell.value, str) and cell.value.startswith("="):
-                feedback.append(f"Formula in cell {cell.coordinate}: {cell.value}")
-            if cell.value and isinstance(cell.value, str) and cell.value.startswith("#"):
-                feedback.append(f"Error in cell {cell.coordinate}: {cell.value}")
-    return feedback
+                formula = cell.value[1:]
+                suggestion = f"For the formula '{formula}' in cell {cell.coordinate}, you can use Python's Pandas library to perform the calculation."
+                suggestions.append(suggestion)
 
-# Main app
-st.title("Excel Business Analysis Reviewer")
-st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Choose a page", ["Home", "Ask Questions"])
+    # Adding general suggestions for business analysis tasks
+    suggestions.append("For data cleaning and preparation, you can use Python's Pandas library to remove outliers, impute missing values, and normalize data.")
+    suggestions.append("For statistical analysis, you can use Python's SciPy library to perform regression analysis, clustering, and classification.")
+    suggestions.append("For machine learning, you can use Python's scikit-learn library to build models like decision trees, random forests, and support vector machines.")
+    suggestions.append("For natural language processing, you can use Python's NLTK library to perform sentiment analysis, topic modeling, and named entity recognition.")
+    suggestions.append("For computer vision, you can use Python's OpenCV library to perform object detection, image classification, and face recognition.")
 
-if page == "Home":
-    st.subheader("Upload Excel File for Analysis")
-    uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
+    return suggestions
 
-    if uploaded_file:
-        st.write("Reviewing the Excel file...")
-        analysis_result = review_excel(uploaded_file)
-        st.subheader("Analysis Results:")
-        st.write(analysis_result)
+def get_openai_feedback(file):
+    # Use OpenAI to provide feedback on Excel skills
+    prompt = f"Review the uploaded Excel file and provide suggestions on how to improve Excel skills. File: {file.name}"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150
+    )
+    return response.choices[0].text
 
-        st.subheader("Formula Check:")
-        formula_feedback = check_formulas(uploaded_file)
-        for item in formula_feedback:
-            st.write(item)
+st.title("Excel Business Analysis Tool")
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 
-elif page == "Ask Questions":
-    st.subheader("Ask Questions About Excel")
-    user_question = st.text_input("Enter your question:")
-    if user_question:
-        st.write("Thank you for your question! We will respond as soon as possible.")
+if uploaded_file:
+    st.write("Reviewing the Excel file...")
+    analysis_result = review_excel(uploaded_file)
+    st.subheader("Analysis Results:")
+    st.write(analysis_result)
 
-# Custom styling
-st.markdown("""
-<style>
-body {
-    color: #4f8bf9;
-    background-color: #f0f0f5;
-    font-family: Arial, sans-serif;
-}
-</style>
-""", unsafe_allow_html=True)
+    st.subheader("Formula Check:")
+    formula_feedback = check_formulas(uploaded_file)
+    for item in formula_feedback:
+        st.write(item)
+
+    st.subheader("Python Integration Suggestions:")
+    python_suggestions = suggest_python_integration(uploaded_file)
+    for suggestion in python_suggestions:
+        st.write(suggestion)
+
+    st.subheader("OpenAI Feedback on Excel Skills:")
+    openai_feedback = get_openai_feedback(uploaded_file)
+    st.write(openai_feedback)
